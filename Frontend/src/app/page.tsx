d1c11ccee46a4c4a624f03b44f80b4a2f005197a
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-01-16 18:38:22 +0100

    Avance seccion 1

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Section01 from "@/components/home/section01/section01";
+import ThemeSwitcher from "@/components/theme/theme-switcher";
 
 export default function Home() {
   return (
     <>
       <Section01/>
+      <ThemeSwitcher/>
     </>
   );
 }
--- Frontend/src/components/home/section01/section01.tsx
+++ Frontend/src/components/home/section01/section01.tsx
@@ -1,15 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import ThemeSwitcher from "@/components/theme/theme-switcher";
 
 export default function Section01() {
   return(
-    <section>
-      <ThemeSwitcher/>
-      <p className="hola">holaaaaa</p>
-      <h1 className="title-day">Titulo</h1>
-      Que tal
-      <Image src={""} alt="Logo"/>
+    <section className="h-screen">
+      <Image src={"/logo.png"} alt="Logo" width={700} height={700}/>
         <Button>Play now!</Button>
       <Image src={""} alt="Gameplay de muestra"/>
     </section>
