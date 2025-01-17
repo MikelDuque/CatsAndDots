dc3071072247e920fcbf15e46493745b12bb02be
Mikel <mikel.0205.navarra@gmail.com>
2025-01-17 12:47:59 +0100

    Arreglo tonterias varias

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -2,13 +2,13 @@ import Section02 from "@/components/home/section02";
 import Section01 from "@/components/home/section01";
 import Section03 from "@/components/home/section03";
 import ThemeSwitcher from "@/components/theme/theme-switcher";
-import Header from "@/components/home/header";
+import Header from "@/components/home/Header"
 
 export default function Home() {
   return (
     <>
     <Header/>
-    <main className="h-screen snap-y snap-mandatory overflow-y-scroll">
+    <main className="h-screen snap-y snap-mandatory overflow-y-scroll no-scrollbar">
       <Section01/>
       <Section02/>
       <Section03/>
--- Frontend/src/components/home/Header.tsx
+++ Frontend/src/components/home/Header.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image"
+  import logo from "@/../public/Home/gato_naranja.png"
 import { Button } from "../ui/button";
 import Title from "../utils/title";
 
 function Header() {
   return(
-    <header className="flex w-screen sticky top-0 justify-between px-10 py-5 drop-shadow-md border-b border-inherit">
+    <header className="h-20 flex w-screen sticky top-0 justify-between p-5 drop-shadow-md border-b border-inherit">
+     <figure className="relative aspect-square h-full">
       <Image 
-      src={"/gato_naranja.png"} 
-      alt={"logo"}
-      height={50}
-      width={50}
-      />
+        src={logo} 
+        alt={"logo"}
+        fill
+        />
+     </figure>
+      
       <Button>
         <Title moreClasses="text-xl">Play now!</Title>
       </Button>
