ae5ab72d08f3317c967917e676f10a6322978c52
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-01-17 21:25:35 +0100

    Creacion proyecto backend
    (quedan cosas por añadir / cambiar en el program.cs)

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,36 +1,100 @@
 
-namespace Backend
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace Backend;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+
+        var builder = WebApplication.CreateBuilder(args);
+        
+        //SERVICES
+        builder.Services.Configure<Settings>(builder.Configuration.GetSection(Settings.SECTION_NAME));
+
+        builder.Services.AddControllers();
+        builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+        builder.Services.AddControllers().AddJsonOptions(options => {options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;});
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                BearerFormat = "JWT",
+                Name = "Authorization",
+                Description = "Ajusta cosas del JWTBearer",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+            options.OperationFilter<SecurityRequirementsOperationFilter>(true, JwtBearerDefaults.AuthenticationScheme);
+        });
+        builder.Services.AddAuthentication().AddJwtBearer(options =>
+        {
+            //Accedemos a la clase settings donde esta el get de JwtKey (Donde se encuentra nuestra clave)
+            Settings settings = builder.Configuration.GetSection(Settings.SECTION_NAME).Get<Settings>()!;
+            //Dicha clave se guarda en la variable key
+            string key = Environment.GetEnvironmentVariable("JWT_KEY");
 
-            app.UseHttpsRedirection();
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                //la unica validacion va a ser la clave
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+            };
+        });
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
 
-            app.UseAuthorization();
+        //Repositorios
 
 
-            app.MapControllers();
+        //Servicios
 
-            app.Run();
+
+        //Mappers
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+        app.UseCors();
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        await SeedDatabase(app.Services);
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"))
+        });
+
+        app.Run();
     }
 }
--- Frontend/src/components/home/section03.tsx
+++ Frontend/src/components/home/section03.tsx
@@ -14,7 +14,7 @@ function Section03() {
           Sé el último en cerrar un cuadro para crear un gato dentro del mismo, y declararlo como tuyo. ¡Al hacerlo, ganarás un turno extra!
         </InfoCard>
         <InfoCard title="Gana puntos" img={paso1}>
-          Por cada gato que crees, sumarás 1 punto a tu puntuación. ¡Monta tu estrategia y encadena "gatos" para alzarte con la victoria!
+          Por cada gato que crees, sumarás 1 punto a tu puntuación. ¡Monta tu estrategia y encadena &quot;gatos&quot; para alzarte con la victoria!
         </InfoCard>
       </div>
     </section>
