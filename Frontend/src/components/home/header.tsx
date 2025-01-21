2826c8f773f64ce735ef9a1ed43e01a5fbcc4ba7
Mikelpladu <mikel.0205.navarra@gmail.com>
2025-01-21 19:50:19 +0100

    Finiquitando los buttons del home

--- Frontend/src/components/home/header.tsx
+++ Frontend/src/components/home/header.tsx
@@ -1,12 +1,18 @@
+"use client"
+
+import { useRouter } from 'next/navigation'
 import Image from "next/image"
   import logo from "@/../public/Home/gato_naranja.png"
 import { Button } from "../ui/button";
 import Title from "../utils/title";
 
 export default function Header() {
+  const router = useRouter();
+  function scrollToTop() {return document.getElementById('section01')?.scrollIntoView({behavior: 'smooth'})};
+
   return(
     <header className="h-14 fixed flex items-center w-screen top-0 justify-between py-1 px-3 shadow-md bg-background">
-     <figure className="relative aspect-square h-4/5">
+     <figure className="relative aspect-square h-4/5" onClick={scrollToTop}>
       <Image 
         src={logo} 
         alt={"logo"}
@@ -14,7 +20,7 @@ export default function Header() {
         />
      </figure>
       
-      <Button>
+      <Button onClick={() => router.push("/menu")}>
         <Title moreClasses="text-sm">Play now!</Title>
       </Button>
     </header>
