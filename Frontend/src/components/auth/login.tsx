1045c078cd919d993a256194bbaa7e75a70f8eca
fernandosanchez1609 <fsanlag686@g.educaand.es>
2025-01-21 19:49:31 +0100

    login

---
+++ Frontend/src/components/auth/login.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+function Login() {
+  return(
+    <Card>
+        <CardHeader>
+          <CardTitle>Inicia Sesión</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Formulario
+        </CardContent>
+      </Card>
+  )
+}
+
+export default Login();
