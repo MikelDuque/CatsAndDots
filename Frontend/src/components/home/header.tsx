b7bfdc3f8322f6acc81bffbde6bd9ad73ddb2fb6
Mikel <mikel.0205.navarra@gmail.com>
2025-01-21 13:02:31 +0100

    Enrutamiento

--- Frontend/src/components/home/header.tsx
+++ Frontend/src/components/home/header.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
   import logo from "@/../public/Home/gato_naranja.png"
 import { Button } from "../ui/button";
-import Title from "./title";
+import Title from "../utils/title";
 
-function Header() {
+export default function Header() {
   return(
     <header className="h-14 fixed flex items-center w-screen top-0 justify-between py-1 px-3 shadow-md bg-background">
      <figure className="relative aspect-square h-4/5">
@@ -18,8 +18,7 @@ function Header() {
         <Title moreClasses="text-sm">Play now!</Title>
       </Button>
     </header>
-
   )
 }
 
-export default Header;
+/* No me deja usar el "navigation" por que dice que es un componente del cliente; pero tampoco el "useRouter" por que dice que no se monta el componente, QUE LECHES? */
