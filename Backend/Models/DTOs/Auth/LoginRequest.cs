0718a0124f6090cb5c11e7a369b659a4619120f6
Mikel <mikel.0205.navarra@gmail.com>
2025-01-21 11:20:10 +0100

    Eliminacion atributo "userName" en "LoginRequest" DTO

--- Backend/Models/DTOs/Auth/LoginRequest.cs
+++ Backend/Models/DTOs/Auth/LoginRequest.cs
@@ -2,7 +2,6 @@ namespace Backend.Models.DTOs;
 
 public class LoginRequest
 {
-  public string UserName {get; set;}
   public string Identifier {get;set;}
   public required string Password {get;set;}
 }
