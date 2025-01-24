8e8b1d1b87eaad02be16da157d4e3ebe9dd35a12
fernandosanchez1609 <fsanlag686@g.educaand.es>
2025-01-24 11:30:34 +0100

    cosas del decodedToken

--- Frontend/src/app/menu/page.tsx
+++ Frontend/src/app/menu/page.tsx
@@ -1,8 +1,14 @@
-export default function Menu() {
+import { getAuth } from "@/features/auth/queries/get-auth";
+
+export default async function Menu() {
+  const { decodedToken } = await getAuth();
+  const username = decodedToken?.unique_name
+
+  console.log(username)
   return (
     <>
     <main>
-      Hola menu
+      Hola {username}
     </main>
     </>
   );
